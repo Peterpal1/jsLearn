2382b89cb86247b53e218b31b61f8bdf7445d672
Silfanus okoth <okothsylvan1@gmail.com>
2024-02-26 18:55:40 +0300

    changes

--- assets/image.js
+++ assets/image.js
@@ -1,5 +1,5 @@
 //parameters in the url
-const accesskey = "123";
+const accesskey = "qqyaC6YzwEy2wdv9cuBYxi0NFlor3Ytu0BMTZ2ytEqw";
 const page = "1";
 const keyword = "nature";
 //promise
@@ -8,6 +8,15 @@ async function getimage(){
     (
        ` https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}`
     );
+    const data = await res.json();
+    data.results.map((item)=>{
+       // console.log(item.urls.regular);
+       let img = document.createElement("img");
+       img.src = item.urls.regular;
+       images.appendChild(img);
+    
+    });
     console.log(res)
+
 }
 getimage();
